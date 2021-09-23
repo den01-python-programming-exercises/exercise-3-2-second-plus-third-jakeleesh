b05ace75bfa4d1001d74f8a06949ffb063d6512d
Jake Lee <jake.lee@fdmgroup.com>
2021-09-23 21:00:59 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    number_list = []
+    while True:
+        num = int(input())
+        if (num == 0):
+            break
+        else:
+            number_list.append(num)
+    print(number_list[1] + number_list[2])
 
 if __name__ == '__main__':
     main()
